55242942f828accbc982f0da15e08e36af05c35c
Gabxxxx <gvs12@discente.ifpe.edu.br>
2024-08-13 13:27:42 -0300

    :refactor: refactor productController and productService

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -40,11 +40,11 @@ export class ProductService {
     }
   }
 
-  async getAllProducts(restaurantId: string): Promise<ApiResponse<Product[]>> {
+  async getAllProducts(categoryId: string): Promise<ApiResponse<Product[]>> {
     try {
-      const restaurantObjectId = new Types.ObjectId(restaurantId);
+      const categoryObjectId = new Types.ObjectId(categoryId);
 
-        const products = await this.productModel.find({ restaurant: restaurantObjectId }).exec();
+        const products = await this.productModel.find({ category: categoryObjectId }).exec();
         return new ApiResponse(true, 'Products found', products.map(product => product.toObject() as Product));
     } catch (error) {
         return new ApiResponse(false, error.message);
